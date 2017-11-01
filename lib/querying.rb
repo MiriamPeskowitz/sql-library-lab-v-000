0a9af86cbb12d2772389e1282822f7c3d03ec697
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-01 22:46:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,10 +22,10 @@ def select_value_and_count_of_most_prolific_species
  end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, series.subgenre_id, sub_genre.name
+  "SELECT authors.name, subgenre.name
   FROM authors
   JOIN series ON authors.id = series.author_id;
-  JOIN subgenres ON series.subgenre_id = subgenre.id;
+  JOIN subgenres ON series.subgenre_id = subgenres.id;
   "
 end
 
