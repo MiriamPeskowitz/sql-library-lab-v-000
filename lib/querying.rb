5d91cc62570ef5bbbbe49bcddb8d9638d6e4ebe0
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-01 23:40:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -47,10 +47,8 @@ end
 
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name
-  FROM characters
-  JOIN character_books
-  ON characters.id = character_books.character_id
+  "SELECT character_if
+  FROM character_books
   JOIN books
   ON character_books.book_id = books.id
   COUNT (characters.name)
